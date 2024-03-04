86cde8b675901c1c2021270ac250756aa1603d14
Neto test <anastasiya.parfinenko@gmail.com>
2024-03-04 11:05:03 +0600

    module.exports

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -6,9 +6,8 @@ test('login', async ({ page }) => {
    await page.getByPlaceholder('Email').fill(email);
    await page.getByPlaceholder('Пароль').fill(password);
    await page.getByTestId('login-submit-btn').click();
-   await expect(page).toHaveURL('https://netology.ru/profile');
+   await expect(page).toHaveURL('https://netology.ru/profile/8341313');
    await expect(page.getByRole('heading', { name: 'Моё обучение' })).toHaveText('Моё обучение');
-
 
 });
 
